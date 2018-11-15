0110848a777891f3e74c0f02f70463475bbbc50b
addai.5 <addai.5@buckeyemail.osu.edu>
2018-11-15 18:07:00 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(int)
+  if int < 2 
+    return false
+  end
+  (2..(int-1)).to_a.each do |ele| 
+    if int % ele == 0
+      return false 
+    else
+  return true 
+    end
+  end
+end
